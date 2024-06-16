0be701008111a0bacdcd326e308d82a8a61b30be
Hervé Darritchon <herve.darritchon@orange.com>
2024-06-16 12:42:26 +0200

    Version Server (WIP). Il faut en fait rajouter des APIs avec des filtres.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,50 @@
-<script>
-import { page } from '$app/stores';
-</script>
-
-<h1>Bienvenue sur le site d'administration du Judo Club</h1>
-
-<ul>
-	<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-		<a href="/">Home</a>
-	</li>
-	<li aria-current={$page.url.pathname === '/inscriptions' ? 'page' : undefined}>
-		<a href="/inscriptions">Inscriptions</a>
-	</li>
-	<li aria-current={$page.url.pathname.startsWith('/membres') ? 'page' : undefined}>
-		<a href="/membres">Membres</a>
-	</li>
-</ul>
+<div class="space-y-10">
+	<header class="flex justify-center items-center gap-10">
+		<img class="w-48" src="logo-app.webp" alt="Post" />
+	</header>
+	<!-- CTA -->
+	<div class="text-center">
+		<a class="btn variant-filled" href="http://localhost:8888/judolateste/wp-admin/" target="_blank">
+			Pour se rendre sur le site du Judo &rarr;
+		</a>
+	</div>
+	<!-- Links -->
+	<div class="max-w-[600px] mx-auto grid grid-cols-1 gap-4">
+		<!-- Client -->
+		<a class="card card-hover overflow-hidden" href="/inscriptions">
+			<header class="flex justify-between items-center p-4">
+				<h3 class="h3">Inscriptions</h3>
+				<h3 class="h3">&rarr;</h3>
+			</header>
+			<hr class="hr" />
+			<div class="p-4 space-y-4">
+				<article>
+					<p>La liste des Inscriptions en cours à traiter.</p>
+				</article>
+			</div>
+		</a>
+		<!-- Server -->
+		<a class="card card-hover overflow-hidden" href="/membres">
+			<header class="flex justify-between items-center p-4">
+				<h3 class="h3">Membres</h3>
+				<h3 class="h3">&rarr;</h3>
+			</header>
+			<hr class="hr" />
+			<div class="p-4 space-y-4">
+				<article>
+					<p>
+						La liste des membres de la saison en cours (2023/2024).
+					</p>
+				</article>
+			</div>
+		</a>
+	</div>
+	<!-- Credit -->
+	<footer class="text-center">
+		<small>
+			<span class="opacity-60">Si vous souhaitez vous rendre directement sur la </span>
+			<a class="anchor" href="http://localhost:8888/judolateste/wp-admin/" target="_blank">partie administration</a>
+			<span class="opacity-60"> du site du Judo Club.</span>
+		</small>
+	</footer>
+</div>
