1452893347ebf9e062fb082432a4377d574a46f8
Hervé Darritchon <herve.darritchon@orange.com>
2024-07-09 15:22:41 +0200

    Change encore un localhost qui trainait.

--- src/routes/inscriptions/+page.ts
+++ src/routes/inscriptions/+page.ts
@@ -1,11 +1,14 @@
 import { HttpAuthenticatedClient } from '$lib/http/HttpAuthenticatedClient';
 import { StorageService } from '$lib/storage/StorageService';
 import { HttpMethod } from '$lib/http/HttpMethod';
+import {
+	PUBLIC_BACKEND_API_URL,
+} from '$env/static/public';
 
 export async function load({ fetch }) {
 	let subscriptions;
 	try {
-		const response = await new HttpAuthenticatedClient('http://localhost:8888/judolateste', new StorageService(localStorage), fetch)
+		const response = await new HttpAuthenticatedClient(PUBLIC_BACKEND_API_URL, new StorageService(localStorage), fetch)
 			.request('/wp-json/associationManagement/v1/subscriptions', HttpMethod.GET);
 		subscriptions = await response.json();
 	} catch (e) {
